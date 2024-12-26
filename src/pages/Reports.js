1b055536e56bea6f72f0ab43b28f174f9b7170c2
1lu29e <1475356850@qq.com>
2024-12-26 20:26:33 +0800

    Add new partners

--- src/pages/Reports.js
+++ src/pages/Reports.js
@@ -232,6 +232,21 @@ const reportsData = [
     project: "OFF THE GRID ",
     englishReport: "https://github.com/DamoclesLabs/GameFi-Analysis-Report/blob/main/OFF%20THE%20GRID/off%20the%20Grid%20Security%20analysis%20report%20.pdf",
     chineseReport: "https://github.com/DamoclesLabs/GameFi-Analysis-Report/blob/main/OFF%20THE%20GRID/Off%20the%20grid%20%E6%B8%B8%E6%88%8F%E5%AE%89%E5%85%A8%E5%88%86%E6%9E%90%E6%8A%A5%E5%91%8A.pdf"
+  },  
+  {
+    project: "Blend",
+    englishReport: "https://github.com/DamoclesLabs/Audit/blob/main/Blend/Final%20Code%20Security%20Audit%20Report%20For%20Blend.pdf",
+    chineseReport: ""
+  },
+  {
+    project: "ThrustPad",
+    englishReport: "https://github.com/DamoclesLabs/Audit/blob/main/ThrustPad/Code%20Security%20Audit%20Report%20For%20ThrustPad%20Final.pdf",
+    chineseReport: ""
+  },
+  {
+    project: "EDBank",
+    englishReport: "https://github.com/DamoclesLabs/Audit/blob/main/EDBank/Code%20Security%20Audit%20Report%20For%20EDBank%20Final.pdf",
+    chineseReport: ""
   },
   // ... 添加其他项目数据
 ];
